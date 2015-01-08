9880834713cdf2385c5121d794cb27f9a2f16a4c
Rob Kayman <rkayman@gmail.com>
2015-01-08 14:05:38 -0800

    Message support for both commands and events
    Support ability to route both events and commands

---
+++ src/proj/CommonDomain.Core/Nothing.cs
@@ -0,0 +1,14 @@
+﻿namespace CommonDomain.Core
+{
+	public class Nothing
+	{
+		public static readonly Nothing Value = new Nothing();
+
+		private Nothing() { }
+
+		public override string ToString()
+		{
+			return "Nothing";
+		}
+	}
+}
